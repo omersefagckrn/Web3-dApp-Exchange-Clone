0cad9e93438cd916fc280f7ef95182d1faa9930a
omersefagckrn <omergckrnx@gmail.com>
2023-03-05 14:07:35 +0300

    font-imported

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
-	<>
+	<StrictMode>
 		<App />
-	</>
+	</StrictMode>
 );
